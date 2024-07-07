576e572202dc4ad659b53babe2db7085087fa6f4
Yadav106 <sahil.jp.yadav@gmail.com>
2024-07-07 15:34:20 +0530

    added gitignore

---
+++ src/youtui/widgets/yt_input.py
@@ -0,0 +1,5 @@
+from textual.app import ComposeResult
+from textual.widgets import Input
+
+class YT_Input(Input):
+    pass
--- src/youtui/youtui.py
+++ src/youtui/youtui.py
@@ -1,12 +1,19 @@
-from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static
+from textual.app import App, ComposeResult, on
+from textual.widgets import Button, Header, Footer, Static
+
+from youtui.widgets.yt_input import YT_Input
 
 class YoutuiApp(App):
     BINDINGS = [
-        ("q", "quit", "Quit")
+        ("q", "quit", "Quit"),
     ]
     def compose(self) -> ComposeResult:
         self.title = "YouTUI"
         yield Header()
-        yield Static("Youtui says Hello")
+        yield YT_Input(placeholder="Video URL")
+        yield Button("Download")
         yield Footer()
+
+    @on(Button.Pressed)
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        self.dark = not self.dark
